b4f9a1f6805204d5eaada2c32f6c8c97e0f05341
AlexKlos <klos@list.ru>
2025-07-16 10:40:59 +0500

    Свяжите числа с кодом

--- server.py
+++ server.py
@@ -13,7 +13,8 @@ from more_itertools import chunked
 
 
 def render_page(template, books, page_path, current_page, total_pages):
-    chunked_books = chunked(books, 2, strict=False)
+    number_of_columns = 2
+    chunked_books = chunked(books, number_of_columns, strict=False)
     rendered_page = template.render(books=chunked_books, 
                                     current_page=current_page, 
                                     total_pages=total_pages)
@@ -70,8 +71,10 @@ def rebuild():
     with open("meta_data.json", "r", encoding="utf-8") as books_file:
         books = json.load(books_file)
 
-    total_pages = (len(books) + 9) // 10
-    chunked_books = chunked(books, 10, strict=False)
+    number_of_rows = 10
+    shift_to_count_the_number_of_pages = number_of_rows - 1
+    total_pages = (len(books) + shift_to_count_the_number_of_pages) // number_of_rows
+    chunked_books = chunked(books, number_of_rows, strict=False)
     for i, ten_books in enumerate(chunked_books, 1):
         page_path = folder_name + '/' + page_name + str(i) + '.html'
         render_page(template, ten_books, page_path, i, total_pages)
